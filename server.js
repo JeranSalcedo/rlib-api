93352674c9f74a671ae62ba8570f67e5a769b0dc
JeranSalcedo <janraymondsalcedo@gmail.com>
2025-03-05 16:28:50 +0800

    Added MongoDB connection

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-// const mysql = require("mysql");
+import connectDB from "./config/db.js";
 
 import logger from "./middleware/logger.js";
 import notFound from "./middleware/notFound.js";
 import errorHandler from "./middleware/error.js";
 
+import users from "./routes/users.js";
 import books from "./routes/books.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -18,22 +19,9 @@ const corsOptions = {
   origin: "http://localhost:3000",
 };
 
-const PORT = process.env.PORT || 5000;
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "rlib",
-// });
+const port = process.env.PORT || 5000;
 
-// connection.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-
-//   console.log("Connected to the database");
-// });
-// global.db = connection;
+connectDB();
 
 app.use(express.json());
 app.use(
@@ -44,9 +32,10 @@ app.use(
 
 app.use(logger);
 
+app.use("/api/users", cors(corsOptions), users);
 app.use("/api/books", cors(corsOptions), books);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
